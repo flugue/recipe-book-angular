02129a6e49c9745ce98bbdf9f2b3828f7a582390
Froilan Lugue <froilan.lugue@orderly.io>
2022-06-22 01:30:15 +0800

    Added initial details for the components.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Pork Adobo', 'Adobo recipe', 'https://panlasangpinoy.com/wp-content/uploads/2009/08/Pork-Adobo--500x375.jpg'),
+    new Recipe('Sinigang', 'Pork Sinigang recipe', 'https://panlasangpinoy.com/wp-content/uploads/2021/08/Killer-Pork-Sinigang.jpg'),
+  ];
 
   constructor() { }
 
